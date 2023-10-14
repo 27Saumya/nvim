0a70294a9fbf7e3898e1d65657fa188dc03c4beb
27Saumya <patelsaumya767@gmail.com>
2023-10-14 14:38:20 +0530

    Configure: powershell as the terminal

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<c-_>", "<cmd>FloatermNew --width=0.7 --height=0.8 --title=⠀⠀Terminal⠀<cr>")
+vim.opt.shell = 'powershell.exe'
+vim.keymap.set("n", "<C-t>", "<cmd>FloatermNew --width=0.7 --height=0.8 --title=⠀⠀Terminal⠀<cr>")
