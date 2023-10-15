8db20ce7e0dfa9cc967c73c30eb8adae92934864
27Saumya <patelsaumya767@gmail.com>
2023-10-15 08:50:00 +0530

    Feat: Add more languages support

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -71,3 +71,63 @@ nvim_lsp.pyright.setup({
     capabilities = capabilities,
     filetypes = {"python"},
   })
+
+nvim_lsp.tsserver.setup {
+	on_attach = on_attach,
+	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+	cmd = { "typescript-language-server", "--stdio" },
+	capabilities = capabilities
+}
+
+nvim_lsp.lua_ls.setup {
+	capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		on_attach(client, bufnr)
+		enable_format_on_save(client, bufnr)
+	end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false
+			},
+		},
+	},
+}
+
+nvim_lsp.tailwindcss.setup {
+	on_attach = on_attach,
+	capabilities = capabilities
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		underline = true,
+		update_in_insert = false,
+		virtual_text = { spacing = 4, prefix = "●" },
+		severity_sort = true,
+	}
+)
+
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = '●'
+	},
+	update_in_insert = true,
+	float = {
+		source = "always", -- Or "if_many"
+	},
+})
